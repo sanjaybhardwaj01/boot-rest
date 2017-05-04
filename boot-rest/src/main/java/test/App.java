6499bcd00a95f512a66fb1c9230dfce8aa16390e
125191 <125191@01HW896175.India.TCS.com>
2017-05-04 12:39:14 +0530

    commit

--- boot-rest/src/main/java/test/App.java
+++ boot-rest/src/main/java/test/App.java
@@ -1,13 +1,12 @@
-package test.boot_rest;
+package test;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+ 
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class App {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
