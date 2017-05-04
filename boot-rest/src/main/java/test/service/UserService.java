55f7888291aba6979b14100127a04aa79cb93998
125191 <125191@01HW896175.India.TCS.com>
2017-05-04 12:40:37 +0530

    New changes

---
+++ boot-rest/src/main/java/test/service/UserService.java
@@ -0,0 +1,25 @@
+package test.service;
+
+import java.util.List;
+
+import test.model.User;
+ 
+public interface UserService {
+     
+    User findById(long id);
+     
+    User findByName(String name);
+     
+    void saveUser(User user);
+     
+    void updateUser(User user);
+     
+    void deleteUserById(long id);
+ 
+    List<User> findAllUsers();
+     
+    void deleteAllUsers();
+     
+    boolean isUserExist(User user);
+     
+}
